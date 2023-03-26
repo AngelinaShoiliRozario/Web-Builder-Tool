dfa7745e891e2c1382b885c735167b1262b7e01c
AngelinaShoiliRozario <shoili.rozario@gmail.com>
2023-03-26 16:36:19 +0600

    26 march 2

--- resources/views/layouts/editComponents/button_editor.blade.php
+++ resources/views/layouts/editComponents/button_editor.blade.php
@@ -1,3 +1,49 @@
-<div style="height: 500px;background:#313131;color:white;width:400px;padding:10px;">
-    <h3 style="text-align: center;text-transform:uppercase;">Button Design</h3>
+<div style="height: 500px;background:#f2f2f2;color:#2f373a;width:400px;padding:0px;">
+    <nav style="background:#2f373a;color:white;padding:10px;">
+        <span
+            style="cursor:pointer;text-transform:uppercase;padding:5px;color:tomato;border:1px solid tomato;">Content</span>
+        <span style="cursor:pointer;text-transform:uppercase;padding:5px;">Design</span>
+    </nav>
+    <div class="button_content">
+        <h3
+            style="text-align: center;text-transform:uppercase;border-top:2px solid gray;background:#2f373a;color:white;padding:10px;margin-bottom:0px!important;">
+            Button Content</h3>
+        <div>
+            <div class="box1">
+                <label for="">Text On button</label>
+                <input type="text" value="Content">
+            </div>
+            <p>Select where button links to</p>
+            <div>
+                <div>
+                    <input type="radio" name="" id="">
+                    <select name="" id="">
+                        <option value="">Insurance Level</option>
+                        <option value="">Insurance Level</option>
+                        <option value="">Insurance Level</option>
+                        <option value="">Insurance Level</option>
+
+                    </select>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+    <div class="button_design">
+        <h3
+            style="text-align: center;text-transform:uppercase;border-top:2px solid gray;background:#2f373a;color:white;padding:10px;margin-bottom:0px!important;">
+            Button Design</h3>
+        <nav>
+            <span>Layout</span>
+            <span>Style</span>
+            <span>Design</span>
+        </nav>
+    </div>
+
 </div>
