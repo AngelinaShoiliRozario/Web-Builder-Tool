0c72c85b76ba3c2c50d2b25124f7118fcde8f963
AngelinaShoiliRozario <shoili.rozario@gmail.com>
2023-03-27 17:03:19 +0600

    27 march 2

--- resources/views/layouts/editComponents/button_editor.blade.php
+++ resources/views/layouts/editComponents/button_editor.blade.php
@@ -1,10 +1,10 @@
-<div style="height: 500px;background:#f2f2f2;color:#2f373a;width:350px;padding:0px;z-index:4000;">
+<div style="background:#f2f2f2;color:#2f373a;width:380px;padding:0px;z-index:4000;box-shadow:0 0 5px gray;">
     <nav style="background:#2f373a;color:white;padding:10px;">
         <span
             style="cursor:pointer;text-transform:uppercase;padding:5px;color:tomato;border:1px solid tomato;">Content</span>
         <span style="cursor:pointer;text-transform:uppercase;padding:5px;">Design</span>
     </nav>
-    <div class="button_content" style="height:100%;display:none;">
+    {{-- <div class="button_content" style="height:100%;display:none;">
         <h3
             style="text-align: center;text-transform:uppercase;border-top:1px solid gray;background:#2f373a;color:white;padding:10px;margin-bottom:0px!important;">
             Button Content</h3>
@@ -111,8 +111,8 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="button_design">
+    </div> --}}
+    <div class="button_design" style="">
         <h3
             style="text-align: center;text-transform:uppercase;border-top:2px solid gray;background:#2f373a;color:white;padding:10px;margin-bottom:0px!important;">
             Button Design</h3>
@@ -122,68 +122,145 @@
             <span style="width: 25%;text-align:center;">Annimation</span>
             <span style="width: 25%;text-align:center;">Spacing</span>
         </nav>
-        <div class="layout" style="padding:10px;cursor:pointer;">
-            <div style="background: white;display:flex;align-items:center;justify-content:center;padding:15px;">
-                <span style="background: #6f8188;padding:10px 40px;border-radius:8px;color:white;">Button</span>
-                <span style="margin-left:50px;">
-                    <i class="fa-solid fa-chevron-right"></i>
-                </span>
+        <div>
+            <div style="height: 500px;overflow:scroll!important;padding:10px;">
+
+                <div class="layout" style="cursor:pointer;margin-bottom:20px;">
+                    <div style="background: white;display:flex;align-items:center;justify-content:center;padding:15px;">
+                        <span style="background: #6f8188;padding:10px 40px;border-radius:8px;color:white;">Button</span>
+                        <span style="margin-left:50px;">
+                            <i class="fa-solid fa-chevron-right"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="style" style="">
+                    <div class="button_style"></div>
+                    <div class="button_text" >
+                        <div style="background: white;padding:15px;margin-bottom:20px;">
+                            <div style="display: flex;justify-content:space-between;">
+                                <label for="" style="width:30%;">Font</label>
+                                <select name="" id="" style="width:60%;padding:3px;">
+                                    <option value="">Abril Falface</option>
+                                    <option value="Arial">Arial</option>
+                                    <option value="Comfortoo">Comfortoo</option>
+                                    <option value="Red Rose">Red Rose</option>
+                                    <option value="Work Sans">Work Sans</option>
+                                </select>
+                            </div>
+                            <div style="display: flex;justify-content:space-between;margin-top:8px;">
+                                <label for="" style="width:30%;">Font Size</label>
+                                <select name="" id="" style="width:60%;padding:3px;">
+                                    @for ($i = 5; $i < 90; $i++)
+                                        <option value="{{ $i }}">{{ $i }}</option>
+                                    @endfor
+                                </select>
+                            </div>
+                            <div style="display: flex;justify-content:space-between;margin-top:8px;">
+                                <label for="" style="width:30%;">Font Color</label>
+                                <div
+                                    style="height: 25px;width:25px;border-radius:50%;border:1.5px solid gray;background:white;">
+                                </div>
+                            </div>
+                            <div style="display: flex;justify-content:space-between;margin-top:25px;">
+                                <label for="" style="width:30%;">Font Format</label>
+                                <div>
+                                    <span
+                                        style="cursor:pointer;padding:8px 13px;background:#f2f2f2;box-shadow: inset 0 0 5px gray;"><i
+                                            class="fa-solid fa-bold"></i></span>
+                                    <span style="cursor:pointer;padding:8px 13px;background:#f2f2f2;"><i
+                                            class="fa-solid fa-underline"></i></span>
+                                    <span style="cursor:pointer;padding:8px 13px;background:#f2f2f2;"><i
+                                            class="fa-solid fa-italic"></i></span>
+    
+                                </div>
+                            </div>
+                            <div style="display: flex;justify-content:space-between;margin-top:25px;">
+                                <label for="" style="width:30%;">Alignment</label>
+                                <div>
+                                    <span style="cursor:pointer;padding:8px 13px;background:#f2f2f2;"><i
+                                            class="fa-solid fa-align-left"></i></span>
+                                    <span
+                                        style="cursor:pointer;padding:8px 13px;background:#f2f2f2;box-shadow:inset 0 0 5px gray;"><i
+                                            class="fa-solid fa-align-center"></i></span>
+                                    <span style="cursor:pointer;padding:8px 13px;background:#f2f2f2;"><i
+                                            class="fa-solid fa-align-right"></i></span>
+    
+                                </div>
+                            </div>
+                            <hr>
+                            <div style="display: flex;justify-content:space-between;margin-top:8px;">
+                                <label for="">Hover Font Color</label>
+                                <div
+                                    style="height: 25px;width:25px;border-radius:50%;border:1.5px solid gray;background:white;">
+                                </div>
+                            </div>
+                            <div style="display: flex;justify-content:space-between;margin-top:25px;">
+                                <label for="">Hover Font Format</label>
+                                <div>
+                                    <span
+                                        style="cursor:pointer;padding:8px 13px;background:#f2f2f2;box-shadow: inset 0 0 5px gray;"><i
+                                            class="fa-solid fa-bold"></i></span>
+                                    <span style="cursor:pointer;padding:8px 13px;background:#f2f2f2;"><i
+                                            class="fa-solid fa-underline"></i></span>
+                                    <span style="cursor:pointer;padding:8px 13px;background:#f2f2f2;"><i
+                                            class="fa-solid fa-italic"></i></span>
+    
+                                </div>
+                            </div>
+                            <hr>
+                            <div style="display:flex;justify-content:center;">
+                                <span style="border:1px solid gray;padding:5px 15px;border-radius:20px;">Reset to theme
+                                    style</span>
+                            </div>
+                        </div>
+                        <div style="background: white;padding:15px;margin-bottom:20px;">
+                        <div style="display:flex;align-items:center;justify-content:space-between;cursor:pointer;padding:10px;">
+                            <span>Animation</span>
+                            <span><i class="fa-solid fa-caret-right"></i></span>
+                        </div>
+                        <div class="animation_subcontent">
+                            <div style="display:flex;align-items:center;gap:10px;">
+                                <span class="icon"><i class="fa-solid fa-computer"></i></span>
+                                <span>Change affects desktop and tablet</span>
+                            </div>
+                              <div class="trigger" style="display:flex;justify-content:space-between;align-items:center;gap:10px;">
+                                    <label for="" style="width:40%;">Trigger</label>
+                                    <select name="" id="" style="width:60%;">
+                                        <option value="entrace">Entrance</option>
+                                        <option value="Scroll">Scroll</option>
+                                    </select>
+                                </div>
+                                <div class="animation">
+                                    <label for="" style="width:30%;">Animation</label>
+                                    <select name="" id="" style="width:60%;">
+                                        <option value="None">None</option>
+                                        <option value="Fade In">Fade In</option>
+                                        <option value="Slide In">Slide In</option>
+                                        <option value="Bounce In">Bounce In</option>
+                                        <option value="Rotate In">Rotate In</option>
+                                        <option value="Roll in">Roll in</option>
+                                        <option value="Zoom In">Zoom In</option>
+                                        <option value="Flip in">Flip in</option>
+                                        <option value="Flash">Flash</option>
+                                        <option value="Pulse">Pulse</option>
+                                        <option value="Shake">Shake</option>
+                                        <option value="Tada">Tada</option>
+                                        <option value="Wobble">Wobble</option>
+                                    </select>
+                                </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="animation">
+
+                </div>
+                <div class="spacing">
+
+                </div>
+
+
             </div>
-        </div>
-        <div class="style" style="padding:10px;">
-            <div class="button_style"></div>
-            <div class="button_text" style="background: white;padding:15px;">
-                <div style="display: flex;justify-content:space-between;">
-                    <label for="" style="width:30%;">Font</label>
-                    <select name="" id="" style="width:60%;padding:3px;">
-                        <option value="">Abril Falface</option>
-                        <option value="Arial">Arial</option>
-                        <option value="Comfortoo">Comfortoo</option>
-                        <option value="Red Rose">Red Rose</option>
-                        <option value="Work Sans">Work Sans</option>
-                    </select>
-                </div>
-                <div style="display: flex;justify-content:space-between;margin-top:8px;">
-                    <label for="" style="width:30%;">Font Size</label>
-                    <select name="" id="" style="width:60%;padding:3px;">
-                        @for ($i = 5; $i < 90; $i++)
-                            <option value="{{ $i }}">{{ $i }}</option>
-                        @endfor
-                    </select>
-                </div>
-                <div style="display: flex;justify-content:space-between;margin-top:8px;">
-                    <label for="" style="width:30%;">Font Color</label>
-                    <div style="height: 25px;width:25px;border-radius:50%;border:1.5px solid gray;background:white;">
-                    </div>
-                </div>
-                <div style="display: flex;justify-content:space-between;margin-top:8px;">
-                    <label for="" style="width:30%;">Font Format</label>
-                    <div>
-                        <span style="padding:8px 13px;background:#f2f2f2;"><i class="fa-solid fa-bold"></i></span>
-                        <span style="padding:8px 13px;background:#f2f2f2;"><i
-                                class="fa-solid fa-underline"></i></span>
-                        <span style="padding:8px 13px;background:#f2f2f2;"><i class="fa-solid fa-italic"></i></span>
-
-                    </div>
-                </div>
-                <div style="display: flex;justify-content:space-between;margin-top:8px;">
-                    <label for="" style="width:30%;">Font Alignment</label>
-                    <div>
-                        <span style="padding:8px 13px;background:#f2f2f2;"><i class="fa-solid fa-bold"></i></span>
-                        <span style="padding:8px 13px;background:#f2f2f2;"><i
-                                class="fa-solid fa-underline"></i></span>
-                        <span style="padding:8px 13px;background:#f2f2f2;"><i class="fa-solid fa-italic"></i></span>
-
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-        <div class="animation">
-
-        </div>
-        <div class="spacing">
-
         </div>
     </div>
 </div>
